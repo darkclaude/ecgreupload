285c000705a88c0f3afc907de1c71cb3fb208f6d
darkclaude <darkclaudevfx@gmail.com>
2016-09-06 13:02:24 +0000

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,16 @@ var Reach = require('./config/models/recharge');
 var mongoose = require('mongoose');
 //var port = 2000;
 var port  = process.env.OPENSHIFT_NODEJS_PORT;
-mongoose.connect(configDB.url2+'/data');
-reachdb = mongoose.createConnection(url2+'/recharges');
+var connection_string = '127.0.0.1:27017/nodekeyz';
+// if OPENSHIFT env variables are present, use the available connection info:
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+
+mongoose.connect("mongodb://"+connection_string+"/data");
+reachdb = mongoose.createConnection("mongodb://"+connection_string+"/recharges");
 var client = express.Router();
 var device = express.Router();
 var stat = express.Router();
@@ -35,5 +43,5 @@ app.get('/*',function(req, res){
  
 
 //app.listen(port);
+console.log("started");
 app.listen(port, process.env.OPENSHIFT_NODEJS_IP);
-console.log("Server Started!");
