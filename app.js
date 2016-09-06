ba318aeec9c58143453a4c7a0622c1857871f6a8
Hamid <darkclaudevfx@gmail.com>
2016-09-06 12:51:21 +0000

    1.0.1

--- app.js
+++ app.js
@@ -1,24 +1,3 @@
-var cluster = require('cluster');
-
-if(cluster.isMaster) {
-    var numWorkers = require('os').cpus().length;
-
-    console.log('Master cluster setting up ' + numWorkers + ' workers...');
-
-    for(var i = 0; i < numWorkers; i++) {
-        cluster.fork();
-    }
-
-    cluster.on('online', function(worker) {
-        console.log('Worker ' + worker.process.pid + ' is online');
-    });
-
-    cluster.on('exit', function(worker, code, signal) {
-        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
-        console.log('Starting a new worker');
-        cluster.fork();
-    });
-} else {
 
 
 
@@ -58,4 +37,3 @@ app.get('/*',function(req, res){
 //app.listen(port);
 app.listen(port, process.env.OPENSHIFT_NODEJS_IP);
 console.log("Server Started!");
-};
