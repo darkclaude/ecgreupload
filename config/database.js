879fb0d7e89a5e6cb7793ab16958c0a8350c21af
Hamid <darkclaudevfx@gmail.com>
2016-09-06 12:38:37 +0000

    df

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
  url : 'mongodb://127.0.0.1:27017/data',
-    url2:  process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    url2:  'mongodb://'+process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
   process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
   process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
   process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
