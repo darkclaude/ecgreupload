ba7246add5408ddc9418e3891fb809d4d27608ad
Hamid <darkclaudevfx@gmail.com>
2016-09-09 21:08:52 +0000

    d

--- views/js/createAccountCtrl.js
+++ views/js/createAccountCtrl.js
@@ -4,7 +4,8 @@
 $scope.create = function(){
 var user = $scope.username;
 var amount = $scope.amount;
-$http.get('/createaccount/'+user+'/'+amount).success(function(data){
+var route = '/createaccount/'+user+'/'+amount;
+$http.get(route).success(function(data){
      
  console.log(data);
  $scope.username= "";
