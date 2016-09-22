767b3b4f8d48e0af94524988b319bbb83ba8d6e8
Hamid <darkclaudevfx@gmail.com>
2016-09-22 22:05:11 +0000

    df

--- views/MapCtrl.js
+++ views/MapCtrl.js
@@ -9,7 +9,7 @@ var lg = 2.295;
 	});
 
 $interval(function() {
-$http.post('/clientapp/getmap/test').success(function(response){
+$http.get('/clientapp/getmap/test').success(function(response){
 mapObj.removeMarker(m);
 lt=response.lat;
 lg =response.lng;
