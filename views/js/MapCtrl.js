f3c6e962aca13d84c82a5a630d4a1fc4125907b0
Hamid <darkclaudevfx@gmail.com>
2016-09-23 14:14:45 +0000

    df

--- views/js/MapCtrl.js
+++ views/js/MapCtrl.js
@@ -12,8 +12,8 @@ var route = '/clientapp/getmap/rest';
 		lng: 0.0
 	});
 var m = mapObj.addMarker({
-lat: 0.0,
-lng: 0.0,
+lat: data.lat,
+lng: data.lng,
 title: 'Phone',
 infoWindow: {
 	content: '<h4>Phone Location</h4><div>HERE</div>',
@@ -22,11 +22,37 @@ infoWindow: {
 });
 
 $interval(function() {
-console.log("me");
-}, 25000);
+$http.post(route).success(function(data){
+	if(data!="none"){
+mapObj.removeMarker(m);
+lt=parseFloat(data.lat);
+lg =parseFloat(data.lng);
+ console.log(lt);
+ console.log(lg);
+  m = mapObj.addMarker({
+lat: data.lat,
+lng: data.lng,
+title: 'Phone',
+infoWindow: {
+	content: '<h4>Phone Location</h4><div>HERE</div>',
+	maxWidth: 100
+}
+});
+}
+else{
+
+}
+
+});
+
+}, 250);
 
 
 
+
+$interval(function() {
+
+}, 2000);
 
 
 
