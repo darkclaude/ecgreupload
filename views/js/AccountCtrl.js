9cada1006acd8368579187e6d74e223fd3d36fbc
Hamid <darkclaudevfx@gmail.com>
2016-09-30 11:34:25 +0000

    df

--- views/js/AccountCtrl.js
+++ views/js/AccountCtrl.js
@@ -125,12 +125,12 @@ console.log(text3AsBytes);
 
 // encrypt...
 var aesCtr = new aesjs.ModeOfOperation.ctr(key,new aesjs.Counter(5));
-var user = aesOfb.encrypt(text1AsBytes);
+var user = aesCtr.encrypt(text1AsBytes);
 //var  aesCtr = new aesjs.ModeOfOperation.ofb(key, iv,8);
 var amount =aesjs.util.convertBytesToString(aesOfb.encrypt(text2AsBytes));
 //var  aesCtr = new aesjs.ModeOfOperation.ofb(key, iv,8);
 var at = aesjs.util.convertBytesToString(aesOfb.encrypt(text3AsBytes));
- var  aesCtr = new aesjs.ModeOfOperation.cfb(key,new aesjs.Counter(5));
+ var  aesCtr = new aesjs.ModeOfOperation.ctr(key,new aesjs.Counter(5));
 var decryptedBytes = aesCtr.decrypt(user);
 
 // Convert our bytes back into text
