7d6095701048099d350db944f6a3795e238c2547
Hamid <darkclaudevfx@gmail.com>
2016-10-26 22:14:26 +0000

    someshit

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ var port  = process.env.OPENSHIFT_NODEJS_PORT;
 var connection_string = ' ';
 // if OPENSHIFT env variables are present, use the available connection info:
   connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":"+process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +process.env.OPENSHIFT_APP_NAME;
-var bodyParser = require('body-parser');
+//var bodyParser = require('body-parser');
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+//app.use( bodyParser.json() );       // to support JSON-encoded bodies
 //app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 //  extended: true
 //})); 
