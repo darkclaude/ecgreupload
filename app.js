d05134453761f5c41f4fde4e40419dd3bd02b08b
Hamid <darkclaudevfx@gmail.com>
2016-12-19 16:45:16 +0000

    df

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ var passport = require('passport');
 var flash = require('connect-flash');
 
 require('./config/passport')(passport);
-//var port = 2500;
-var port  = process.env.OPENSHIFT_NODEJS_PORT;
+var port = 2500;
+//var port  = process.env.OPENSHIFT_NODEJS_PORT;
 var connection_string = ' ';
 // if OPENSHIFT env variables are present, use the available connection info:
   connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":"+process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +process.env.OPENSHIFT_APP_NAME;
@@ -86,7 +86,7 @@ app.get('/*',function(req, res){
         
  
 
-//app.listen(port);
+app.listen(port);
 console.log("started");
-app.listen(port, process.env.OPENSHIFT_NODEJS_IP);
+//app.listen(port, process.env.OPENSHIFT_NODEJS_IP);
 
